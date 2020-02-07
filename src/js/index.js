d88b8c2ada8047f8c2e2f38a28313fd41f279289
Diogo Silva <diogo.soares.silva@gmail.com>
2020-02-07 10:00:48 +0100

    it moves

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('it works');
 });
 
-document.addEventListener('click', () => {
-  const pacmanClick = document.querySelector('.entity--pac');
-  // pacmanClick.style.backgroundPositionX = 'right';
-  pacmanClick.classList.toggle('entity--pac--pacChange');
-});
+let xpos = 0;
+
+
+document.addEventListener('keydown', (e) => {
+  if (e.keyCode === 39) {
+    console.log('right key pressed!')
+    const pacmanClick = document.querySelector('.entity--pac');
+    pacmanClick.classList.toggle('entity--pac--pacChange');
+    xpos += 85;
+    pacmanClick.style.left = xpos + 'px';
+  }
+})
+
+
+
+// Create a class called Pacman which will hold the x position and mouth closed/opened state. For the closed/opened state use a variable called mouth.
+// Create a method moveRight() inside your class that will move the pacman to the right.
+// Create an update() function that will update the pacman's position on the screen.
